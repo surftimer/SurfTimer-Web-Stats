37ce9294743e585bb5f1c9427b596bea3b0e33ec
Bara <mail@bara.dev>
2021-01-05 18:59:47 +0100

    Add german translation

---
+++ inc/languages/german.php
@@ -0,0 +1,59 @@
+<?php
+/*
+    English language for Surftimer-Web-Stats v1.15.Beta
+*/
+
+define('DASHBOARD', 'Dashboard');           // Dashboard
+define('TOP_PLAYERS', 'Top Spieler');       // Top Players
+define('MAPS', 'Karten');                     // Maps
+define('MOST_ACTIVE', 'Am Aktivsten');       // Most Active
+define('TOTAL_PLAYERS', 'Spieler insgesamt');   // Total Players
+define('TOTAL_MAPS', 'Karten insgesamt');         // Total Maps
+define('TOTAL_COPT', 'Spielzeit aller Spieler');   // Total count of Players Times
+define('ANY_STYlE', 'alle Stile');           // any style
+define('NORMAL_STYLE', 'normal Stil');     // normal style
+define('RECENT_10_RECORDS', 'Letzten 10 Rekorde');   // Recent 10 Records
+define('TOP10P', 'Top 10 Spieler');         // Top 10 Players
+define('TOP10WRH', 'Top 10 WR Halter');    // Top 10 WR Holders
+define('TOP10BWRH', 'Top 10 Bonus WR Halter'); // Top 10 Bonus WR Holders
+define('TOP10SWRH', 'Top 10 Stage WR Halter'); // Top 10 Stage WR Holders
+define('TOP', 'TOP');                      // TOP
+define('PLAYERS', 'Spieler');               // Players
+define('MAPS_COLLECTION', 'Kartenliste'); // Map Collection
+define('MOST_ACTIVE_DESCRIPTION', 'Zeige Spieler mit mehr als 2 Stunden Spielzeit.'); // Showing players with more than two hours of playtime.
+define('NSMC', 'Rekorde im normalen Stil'); // Normal Style Map Completions
+define('STCNS', 'TOP Stage Rekorde im normalen Stil');  // Stage TOP completions Normal style
+define('NSBC', 'Rekorde im normalen Bonus Stil');     // Normal Style Bonuses Completions
+define('TYPE', 'Art');                     // Type
+define('TIER', 'Schwierigkeit');                     // Tier
+define('BONUS', 'Bonus');                   // Bonus
+define('BONUSES', 'Bonusse');               // Bonuses
+define('MAX_VELOCITY', 'Max Geschwindigkeit');     // Max Velocity
+define('TOTAL_COMPLETIONS', 'Anzahl Rekorde');   // Total Completions
+define('NSC', 'Anzahl normal Stil Rekode');          // Normal Style Completions
+define('BTC', 'Anzahl Bonus Rekorde');         // Bonuses Total Completions
+define('BNSC', 'Anzahl normale Bonus Rekorde'); // Bonuses Normal Style Completions
+define('USERNAME', 'Spielername');                 // Username
+define('MAP_NAME', 'Kartenname');                 // Map name
+define('TIME', 'Zeit');                         // Time
+define('DATE', 'Datum');                         // Date
+define('POINTS', 'Punkte');                     // Points
+define('STAGES', 'Stages');                     // Stages
+define('WRS', 'WRs');                           // WRs
+define('FINISHED_MAPS', 'Beendete Karten');       // Finished Maps
+define('FINISHED_BONUSES', 'Beendete Bonusse');       // Finished Bonuses
+define('FINISHED_STAGES', 'Beendete Stages');       // Finished Stages
+define('STEAM_PROFILE', 'Steam Profil');       // Steam Profile
+define('HOURS_PLAYED', 'Stunden gespietl');         // Hours Played
+define('CONNECTIONS', 'Verbindungen');           // Connections
+define('LAST_SEEN', 'Zuletzt gesehen');               // Last Seen
+define('JOINED', 'Zuerst gesehen');                     // Joined
+define('MOST_ACTIVE_DISABLED', 'Am Aktivsten Modul ist deaktiviert.');                  // Most Active module is disabled.
+define('MOST_ACTIVE_ACTIVATE', 'Zum aktivieren bitte den Administrator kontaktieren.');     // For activation please contact administrator.
+define('MAP_NOT_FOUND', 'wurde in der Datenbank nicht gefunden.');   // were not found in our database.
+define('MAP', 'Karte');                     // Map
+define('STAGE', 'Stage');                     // Stage
+define('COMPLETIONS', 'Rekorde');        // Connections
+define('TOP_PLAYER_NAME', 'Spielername');        // Top Player Name
+define('TOP_TIME', 'Beste Zeit');        // Top Time
+define('TOTAL_STAGE_COMP', 'Anzahl Stage Rekorde');        // Total Stage Completions
