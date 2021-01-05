a43a3cd40396361baf0792fe0d7fd68fc591516d
qawery-just-sad <53490397+qawery-just-sad@users.noreply.github.com>
2021-01-05 17:17:35 +0100

    Polish translation

---
+++ inc/languages/polish.php
@@ -0,0 +1,59 @@
+<?php
+/*
+    Polish language for Surftimer-Web-Stats v1.15.Beta
+*/
+
+define('DASHBOARD', 'Główny Panel');           // Dashboard
+define('TOP_PLAYERS', 'Najlepsi Gracze');       // Top Players
+define('MAPS', 'Mapy');                     // Maps
+define('MOST_ACTIVE', 'Najbardziej Aktywni');       // Most Active
+define('TOTAL_PLAYERS', 'Wszyscy Gracze');   // Total Players
+define('TOTAL_MAPS', 'Wszystkie Mapy');         // Total Maps
+define('TOTAL_COPT', 'Łączny czas wszystkich graczy');   // Total count of Players Times
+define('ANY_STYlE', 'Jakikolwiek Styl');           // any style
+define('NORMAL_STYLE', 'Styl Normalny');     // normal style
+define('RECENT_10_RECORDS', '10 Ostatnich Rekordów');   // Recent 10 Records
+define('TOP10P', 'Top 10 Graczy');         // Top 10 Players
+define('TOP10WRH', 'Top 10 Rekordzistów');    // Top 10 WR Holders
+define('TOP10BWRH', 'Top 10 Bonus Rekordzistów'); // Top 10 Bonus WR Holders
+define('TOP10SWRH', 'Top 10 Etap Rekordzistów'); // Top 10 Stage WR Holders
+define('TOP', 'Top');                      // TOP
+define('PLAYERS', 'Graczy');               // Players
+define('MAPS_COLLECTION', 'Kolekcja Map'); // Map Collection
+define('MOST_ACTIVE_DESCRIPTION', 'Ukazuje graczy którzy grali więcej niż 2 godziny.'); // Showing players with more than two hours of playtime.
+define('NSMC', 'Ukończenia Mapy w stylu Normalnym'); // Normal Style Map Completions
+define('STCNS', 'Top ukończenia etapów w stylu Normalnym');  // Stage TOP completions Normal style
+define('NSBC', 'Ukończenia Bonusu w stylu Normalnym');     // Normal Style Bonuses Completions
+define('TYPE', 'Typ');                     // Type
+define('TIER', 'Tier');                     // Tier
+define('BONUS', 'Bonus');                   // Bonus
+define('BONUSES', 'Bonusy');               // Bonuses
+define('MAX_VELOCITY', 'Maksymalna Prędkość');     // Max Velocity
+define('TOTAL_COMPLETIONS', 'Wszystkie Ukończenia');   // Total Completions
+define('NSC', 'Ukończenia w stylu Normalnym');          // Normal Style Completions
+define('BTC', 'Wszystkie Ukończenia Bonusu');         // Bonuses Total Completions
+define('BNSC', 'Ukończenia Bonusu w stylu Normalnym'); // Bonuses Normal Style Completions
+define('USERNAME', 'Nazwa Użytkownika');                 // Username
+define('MAP_NAME', 'Nazwa Mapy');                 // Map name
+define('TIME', 'Czas');                         // Time
+define('DATE', 'Data');                         // Date
+define('POINTS', 'Punkty');                     // Points
+define('STAGES', 'Etapy');                     // Stages
+define('WRS', 'Rekordy');                           // WRs
+define('FINISHED_MAPS', 'Ukończone Mapy');       // Finished Maps
+define('FINISHED_BONUSES', 'Ukończone Bonusy');       // Finished Bonuses
+define('FINISHED_STAGES', 'Ukończone Etapy');       // Finished Stages
+define('STEAM_PROFILE', 'Profil Steam');       // Steam Profile
+define('HOURS_PLAYED', 'Godziny spędzone');         // Hours Played
+define('CONNECTIONS', 'Połączenia');           // Connections
+define('LAST_SEEN', 'Ostatnio widziany');               // Last Seen
+define('JOINED', 'Dołączył');                     // Joined
+define('MOST_ACTIVE_DISABLED', 'Moduł najbardziej aktywnych graczy jest wyłączony.');                  // Most Active module is disabled.
+define('MOST_ACTIVE_ACTIVATE', 'Proszę skontaktować się z administratorem strony.');     // For activation please contact administrator.
+define('MAP_NOT_FOUND', 'nie znaleziono w bazie Rekordówych.');   // were not found in our database.
+define('MAP', 'Mapa');                     // Map
+define('STAGE', 'Etap');                     // Stage
+define('COMPLETIONS', 'Ukończenia');        // Connections
+define('TOP_PLAYER_NAME', 'Nazwa Użytkownika');        // Top Player Name
+define('TOP_TIME', 'Najlepszy Czas');        // Top Time
+define('TOTAL_STAGE_COMP', 'Wszystkie Ukończenia Etapu');        // Total Stage Completions
