9d8150b4ec24af5987593c2ddb753b74af76b454
KristianP26 <partl.kristian@gmail.com>
2022-05-07 19:35:07 +0200

    Removing SurfCommunity Stuff 3

--- inc/database.php
+++ inc/database.php
@@ -1,7 +1,7 @@
 <?php
 
 // Create connection
-$db_conn_surftimer = new mysqli($db_host.':'.$db_port, $db_username, $db_password, $db_database_surftimer);
+$db_conn_surftimer = new mysqli($db_host.':'.$db_port, $db_username, $db_password, $db_database);
 
 // Check connection
 if ($db_conn_surftimer->connect_error) {
@@ -11,18 +11,6 @@ if ($db_conn_surftimer->connect_error) {
     $db_conn_surftimer_status = 1;
 };
 
-// Create connection
-$db_conn_web = new mysqli($db_host.':'.$db_port, $db_username, $db_password, $db_database_web);
-
-// Check connection
-if ($db_conn_web->connect_error) {
-    die("<center><h2>Please contact site Administrator.</h2><br><h3>DB: Connection failed:<br>"
-     . $db_conn_web->connect_error . "</h3></center>");
-} else {
-    $db_conn_web_status = 1;
-};
-
 
 $db_conn_surftimer -> set_charset("utf8mb4");
-$db_conn_web -> set_charset("utf8mb4");
 
--- navbar.php
+++ navbar.php
@@ -29,7 +29,7 @@
 
     <nav class="container navbar navbar-expand-lg navbar-light bg-light py-3 px-5 shadow-sm">
         <div class="container">
-            <a class="navbar-brand" href=""><i class="fab fa-kickstarter-k"></i> Kiepownica</a>
+            <a class="navbar-brand" href="">SurfSats</a>
             <!--<a class="navbar-brand d-none d-md-block" href="https://surfcommunity.eu/"><img src="./images/logo_navbar.svg" height="40"></a>-->
             <!--<a class="navbar-brand d-md-none" href="https://surfcommunity.eu/"><img src="./images/logo_navbar_1.svg" height="40"></a>-->
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#SurfCommunityNavBar" aria-controls="SurfCommunityNavBar" aria-expanded="false" aria-label="Toggle navigation">
@@ -46,7 +46,6 @@
                         </a>
                         <div class="dropdown-menu dropdown-menu-right shadow-sm" aria-labelledby="Dashboard-Na v-Dropdown">
                             <a class="dropdown-item <?php echo nav_active('Home - Dashboard'); ?>" href="dashboard-home.php"><i class="fas fa-home"></i> Home</a>
-                            <a class="dropdown-item <?php echo nav_active('Servers - Dashboard'); ?>" href="dashboard-servers.php"><i class="fas fa-server"></i> Servers</a>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="#" data-toggle="modal" data-target="#SearchPlayers"><i class="fas fa-search"></i> Search Player</a>
                             <div class="dropdown-divider"></div>
@@ -57,47 +56,8 @@
 
                         </div>
                     </li>
-                    <li class="nav-item <?php echo nav_active('Donate'); ?>">
-                        <a class="nav-link" href="donate-old.php"><i class="fas fa-donate"></i> Donate</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle <?php if(in_array($page_name, $nav_active_dropdown_other)) echo 'active'; ?>" href="#" id="Other-Nav-Dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-bars"></i> Other
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right shadow-sm" aria-labelledby="Other-Nav-Dropdown">
-                            <a class="dropdown-item <?php echo nav_active('About'); ?>" href="about.php"><i class="fas fa-info-circle"></i> About</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="http://discord.surfcommunity.eu/" target="_blank"><i class="fab fa-discord"></i> Discord</a>
-                            <a class="dropdown-item" href="http://steam.surfcommunity.eu/" target="_blank"><i class="fab fa-steam"></i> Steam Group</a>
-                            <div class="dropdown-divider"></div>
-                            <h6 class="dropdown-header">In Game</h6>
-                            <a class="dropdown-item <?php echo nav_active('Rules'); ?>" href="rules.php"><i class="fas fa-list-ul"></i> Rules</a>
-                            <a class="dropdown-item <?php echo nav_active('Commands'); ?>" href="commands.php"><i class="fas fa-keyboard"></i> Commands</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="http://status2.kristianpartl.eu/" target="_blank"><i class="fas fa-server"></i> Server Status</a>
-                            <a class="dropdown-item" href="https://kiepownica.pl" target="_blank"><i class="fas fa-globe-europe"></i> Kiepownica</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item <?php echo nav_active('Terms of Service'); ?>" href="terms-of-service.php"><i class="fas fa-info-circle"></i> Terms of Service</a>
-                            <a class="dropdown-item <?php echo nav_active('Privacy Policy'); ?>" href="privacy-policy.php"><i class="fas fa-user-lock"></i> Privacy Policy</a>
-                        </div>
-                    </li>
+                
                 </ul>
-                <?php if(!$steam->loggedIn()): ?>
-                    <button type="button" class="btn btn-sm btn-outline-primary ml-3 shadow-sm" data-toggle="modal" data-target="#SteamLogin">
-                        <i class="fas fa-sign-in-alt"></i> Sign in
-                    </button>
-                <?php else: ?>
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-primary ml-3 shadow-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-user"></i> <?php echo $steam->personaname; ?>
-                        </button>
-                        <div class="dropdown-menu dropdown-menu-right shadow-sm">
-                            <a class="dropdown-item" href="dashboard-player.php?id=<?php echo $user_communityid; ?>"><i class="far fa-id-card"></i> Profile</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="?logout"><i class="fas fa-sign-out-alt"></i> Sign Out</a>
-                        </div>
-                    </div>
-                <?php endif; ?>
             </div>
         </div>
     </nav>
@@ -106,30 +66,6 @@
 
     <div class="container bg-white shadow-lg">
         <div id="page-content">
-    <?php if(!$steam->loggedIn()): ?>
-        <!-- Login Modal -->
-        <div class="modal shadow fade" id="SteamLogin" tabindex="-1" aria-labelledby="SteamLoginLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header shadow-sm">
-                        <h5 class="modal-title" id="SteamLoginLabel"><i class="fas fa-sign-in-alt"></i> Sign in</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body text-center">
-                        <p>Please use the following button to sing in</p>
-                        <a role="button" class="btn btn-dark btn-sm shadow-sm" href="<?php echo $steam->loginUrl(); ?>">Sign in through<br> <i class="fab fa-steam"></i> STEAM</a>
-                    </div>
-                    <div class="modal-footer shadow-sm">
-                        <small class="text-muted">
-                            <span class="text-dark">Surf Community</span> is a community website and is not affiliated with <a href="https://www.valvesoftware.com/" target="_blank" title="Valve Software">Valve</a> or <a href="https://steampowered.com/" target="_blank" title="Steam Powered">Steam</a>.
-                        </small>
-                    </div>
-                </div>
-            </div>
-        </div>
-    <?php endif; ?>
 
     <!-- Search Modal -->
     <div class="modal shadow fade" id="SearchPlayers" tabindex="-1" aria-labelledby="SearchPlayersLabel" aria-hidden="true">
