30fd7693faeed8775dd8cdf402dffcde26f6862b
KristianP26 <partl.kristian@gmail.com>
2022-05-10 15:19:46 +0200

    Changed version number to 2.02

--- footer.php
+++ footer.php
@@ -1,25 +1,25 @@
-        </div>
-        <div class="mt-2">
-            <hr>
-            <div class="row">
-                <div class="text-center text-body col-lg-6 offset-lg-3">
-                    <p>
-                        Copyright &copy; 2021-<?php echo date("Y");?>  - Surftimer-Web-Stats
-                        <br>
-                        <small>
-                            Coded & Designed with ❤ by <a class="text-dark" href="https://github.com/KristianP26/" taget="_blank">Kristián Partl</a>.
-                            <br class="py-0">
+            </div>
+            <div class="mt-2">
+                <hr>
+                <div class="row">
+                    <div class="text-center text-body col-lg-6 offset-lg-3">
+                        <p>
+                            Copyright &copy; 2021-<?php echo date("Y");?>  - Surftimer-Web-Stats
+                            <br>
                             <small>
-                                All times are in <a class="text-dark" href="https://time.is/en/<?php echo $mysql_server_timezone; ?>" target="_blank"><?php echo $mysql_server_timezone; ?></a>.
+                                Coded & Designed with ❤ by <a class="text-dark" href="https://github.com/KristianP26/" taget="_blank">Kristián Partl</a>.
+                                <br class="py-0">
+                                <small>
+                                    All times are in <a class="text-dark" href="https://time.is/en/<?php echo $mysql_server_timezone; ?>" target="_blank"><?php echo $mysql_server_timezone; ?></a>.
+                                </small>
                             </small>
-                        </small>
-                        <br>
-                        <small><a class="text-dark" href="https://github.com/KristianP26/Surftimer-Web-Stats" title="Kristián Partl Github" target="_blank">Surftimer-Web-Stats</a> v.2.01</small>
-                    </p>
+                            <br>
+                            <small><a class="text-dark" href="https://github.com/KristianP26/Surftimer-Web-Stats" title="Kristián Partl Github" target="_blank">Surftimer-Web-Stats</a> v.2.02</small>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "16cdd8546abf4524947931a521439fca"}'></script><!-- End Cloudflare Web Analytics -->
     </body>
+    <!-- Coded & Designed with ❤ by Kristián Partl.  --->
 </html>
