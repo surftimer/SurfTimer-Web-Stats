fce7f5f056ba1f0c4f9042dcfbefc5a54995b31f
Kristián Partl <partl.kristian@gmail.com>
2022-08-08 22:57:34 +0200

    Fixed config and readme file (#29)

--- inc/docker_conf.php
+++ inc/docker_conf.php
@@ -23,9 +23,11 @@
     $settings_favicon       = $_ENV["SETTINGS_FAVICON"] ?? $settings_favicon;
     $settings_navbar_logo   = $_ENV["NAVBAR_LOGO"] ?? $settings_navbar_logo;
     $settings_navbar_title  = $_ENV["NAVBAR_TITLE"] ?? $settings_navbar_title;
+    $settings_map_link_icon = $_ENV["MAP_LINK_ICON"] ?? $settings_map_link_icon;
     $settings_custom_link_name     = $_ENV["CUSTOM_LINK_NAME"] ?? $settings_custom_link_name;
     $settings_custom_link_adress   = $_ENV["CUSTOM_LINK_ADDRESS"] ?? $settings_custom_link_adress;
     $settings_custom_link_2_name     = $_ENV["CUSTOM_LINK_NAME_2"] ?? $settings_custom_link_2_name;
     $settings_custom_link_2_adress   = $_ENV["CUSTOM_LINK_ADDRESS_2"] ?? $settings_custom_link_2_adress;
     $settings_custom_link_3_name     = $_ENV["CUSTOM_LINK_NAME_3"] ?? $settings_custom_link_3_name;
     $settings_custom_link_3_adress   = $_ENV["CUSTOM_LINK_ADDRESS_3"] ?? $settings_custom_link_3_adress;
+    $settings_player_flags           = $_ENV["PLAYER_FLAGS"] ?? $settings_player_flags;
