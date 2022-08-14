3ae0161bb2be374c9e41a8464f210e1088e339be
shipyy <jonny.blue@live.com.pt>
2022-08-14 22:06:49 +0100

    --portuguese translation

---
+++ inc/languages/Portuguese.php
@@ -0,0 +1,140 @@
+<?php
+/*
+    English language for Surftimer-Web-Stats v2.06
+    Translation by: KristiánP
+*/
+
+define('NAVBAR_DASHBOARD', 'Painel');
+define('NAVBAR_HOME', 'Página Principal');
+define('NAVBAR_TOP_PLAYERS', 'Jogadores de Topo');
+define('NAVBAR_MAPS', 'Mapas');
+define('NAVBAR_MOST_ACTIVE', 'Mais Activos');
+define('NAVBAR_RECENT_RECORDS', 'Recordes Recentes');
+define('NAVBAR_SEARCH_PLAYER', 'Procurar Jogador');
+
+define('SEARCH_SEARCH_PLAYERS', 'Procurar Jogadores');
+define('SEARCH_CLOSE', 'Fechar');
+define('SEARCH_INPUT', 'Procurar Jogadores pelo Nome de Utilizador, SteamID or SteamID64');
+define('SEARCH_NO_RESULTS', 'Sem Resultados');
+
+define('HOME_TOTAL_PLAYERS', 'Total de Jogadores');
+define('HOME_TOTAL_MAPS', 'Total de Mapas');
+define('HOME_TOTAL_BONUSES', 'Total de Bónus');
+define('HOME_TOTAL_COMPLETIONS', 'Total de Finalizações');
+define('HOME_HOURS_PLAYED', 'Horas Jogadas');
+define('HOME_RECENT', 'Últimos 10 Records de Mapas');
+define('HOME_TOP_PLAYERS', 'Top 10 de Jogadores');
+define('HOME_TOP_WR', 'Top 10 de Jogadores com Recordes de Mapas');
+define('HOME_TOP_BONUS_WR', 'Top 10 de Jogadores com Recordes de Bónus');
+define('HOME_TOP_STAGE_WR', 'Top 10 de Jogadores com Recordes de Etapas');
+define('HOME_RECENT_MAPS', 'Mapas Adicionados Recentemente');
+define('HOME_BUTTON_SHOW_MORE', 'Ver Mais');
+
+define('TABLE_USERNAME', 'Nome de Utilizador');
+define('TABLE_MAP', 'Mapa');
+define('TABLE_MAPS', 'Mapas');
+define('TABLE_MAP_NAME', 'Nome do Mapa');
+define('TABLE_TIME', 'Tempo');
+define('TABLE_DATE', 'Data');
+define('TABLE_POINTS', 'Pontos');
+define('TABLE_BONUS', 'Bónus');
+define('TABLE_BONUSES', 'Bónus');
+define('TABLE_NO_BONUS', 'Sem Bónus');
+define('TABLE_STAGE', 'Etapa');
+define('TABLE_STAGES', 'Etapas');
+define('TABLE_COMPLETIONS', 'Finalizações');
+define('TABLE_WRS', 'WRs');
+define('TABLE_FINISHED_MAPS', 'Mapas Finalizados');
+define('TABLE_FINISHED_BONUSES', 'Bónus Finalizados');
+define('TABLE_FINISHED_STAGES', 'Etapas Finalizadas');
+define('TABLE_TIER', 'Tier');
+define('TABLE_TYPE', 'Tipo');
+define('TABLE_ADDED', 'Adicionado');
+define('TABLE_JOINED', 'Entrou');
+define('TABLE_HOURS', 'Horas');
+define('TABLE_RANK', 'Posição');
+define('TABLE_BONUS_RANK', 'Posição de Bónus');
+define('TABLE_STAGE_RANK', 'Posiçãde de Etapa');
+define('TABLE_RUNTIME', 'Tempo');
+define('TABLE_CONNECTIONS', 'Conexões');
+define('TABLE_LAST_SEEN', 'Última vez visto');
+define('TABLE_LINEAR', 'Linear');
+define('TABLE_STAGED', 'Com Etapas');
+define('TABLE_NULL', 'Nulo');
+define('TABLE_TODAY', 'Hoje');
+define('TABLE_YESTERDAY', 'Ontem');
+define('TABLE_DAYS_AGO', 'dias atrás');
+define('TABLE_TOP_PLAYER_NAME', 'Nome do Jogador do Topo');
+define('TABLE_TOP_TIME', 'Tempo do Topo');
+define('TABLE_TOTAL_STAGE_COMPLETIONS', 'Total de finalizções de etapas');
+
+define('MAPS_MAP_COLLECTION', 'Coleção de Mapas');
+define('MAPS_MAX_VELOCITY', 'Velocidade Máxima');
+define('MAPS_TOTAL_COMPLETIONS', 'Total Finalizações');
+define('MAPS_MAP_COMPLETIONS', 'Finalizações do Mapa');
+define('MAPS_STAGE_COMPLETIONS', 'Finalizações da Etapa');
+define('MAPS_BONUSES_COMPLETIONS', 'Finalizações do Bónus');
+
+define('PROFILE_PLAYER_PROFILE', 'Perfil do Jogador');
+define('PROFILE_TOP', 'TOP');
+define('PROFILE_USER_TOTAL_COMPLETION_PROGRESS', 'Progresso Total de finalizações do Jogador');
+define('PROFILE_OVERALL_RANK', 'Classificação Geral');
+define('PROFILE_TOTAL_PLAYTIME', 'Tempo Total de Jogo');
+define('PROFILE_SURF_TIME', 'Tempo a Surfar');
+define('PROFILE_MAP_COMPLETIONS', 'Finalizações de Mapas');
+define('PROFILE_BONUS_COMPLETIONS', 'Finalizações de Bónus');
+define('PROFILE_STAGES_COMPLETIONS', 'Finalizações de Etapas');
+define('PROFILE_MAP_TOP_10S', 'Mapas Com Top 10');
+define('PROFILE_BONUS_WR_POINTS', 'Pontos dos WR de Bónus');
+define('PROFILE_MAP_TOP_10S_POINTS', 'Pontos dos TOP10 de mapas');
+define('PROFILE_MAP_POINTS', 'Pontos de Mapas');
+define('PROFILE_TOTAL_POINTS', 'Total de Pontos');
+define('PROFILE_TOTAL_CONNECTIONS', 'Total de Conexões');
+define('PROFILE_SPEC_TIME', 'Tempo de Espectador');
+define('PROFILE_MAP_RECORDS', 'Recordes de Mapas');
+define('PROFILE_BONUS_RECORDS', 'Recordes de Bónus');
+define('PROFILE_STAGE_RECORDS', 'Recordes de Etapas');
+define('PROFILE_MAP_WR_POINTS', 'Map WR Points');
+define('PROFILE_STAGE_WR_POINTS', 'Pontos de WR de Etapas');
+define('PROFILE_GROUP_POINTS', 'Pontos de Grupos');
+define('PROFILE_BONUS_POINTS', 'Pontos de Bónus');
+define('PROFILE_HRS', 'hrs');
+define('PROFILE_TIER', 'Tier');
+define('PROFILE_PLAYER_COMPLETIONS_BY_MAP_TIER', 'Finalizações do Jogador pela Tier de Mapas');
+define('PROFILE_PLAYER_FINISHED_MAPS', 'Mapas Finalizados do Jogador');
+define('PROFILE_PLAYER_FINISHED_BONUSES', 'Bónus Finalizados do Jogador');
+define('PROFILE_PLAYER_FINISHED_STAGES', 'Etapas Finalizadas do Jogador');
+
+define('TOP_1000_PLAYERS', 'Topo de 1,000 Jogadores');
+define('MAP_COLLECTION', 'Coleção de Mapas');
+define('MOST_ACTIVE', 'Mais Ativos');
+define('RECENT_100_MAP_RECORDS', 'Últimos 100 Recordes de Mapas');
+
+define('LOADING_MAP', 'A Carregar o Mapa:');
+define('DETAILS', 'Detalhes');
+define('PLEASE_WAIT', 'Por favor, espere');
+define('LOADING', 'A Carregar');
+define('LOADING_MAP_COLLECTION', 'A Carregar a Coleção de Mapas');
+define('LOADING_MOST_ACTIVE_LIST', 'A Carregar a Lista dos Mais Ativos');
+define('LOADING_PLAYER_PROFILE', 'A Carregar o Perfil do Jogador');
+define('LOADING_RECENT_MAP_RECORDS_LIST', 'A Carregar a Lista dos Últimos Recordes de Mapas');
+define('LOADING_TOP_PLAYERS_LIST', 'A Carregar a Lista dos Jogadores de Topo');
+define('MAP_NOT_ADDED_PROPERLY', 'não foram adicionados corretamente ao servidor, por favor contacta o Administrador do servidor');
+define('NOT_FOUND_IN_DB', 'não foram encontrados na nossa base de dados');
+define('ALL_TIMES_ARE_IN', 'Todas as datas estão em');
+
+define('DATATABLES_processing', 'A Processar...');
+define('DATATABLES_search', 'Procurar:');
+define('DATATABLES_lengthMenu', 'Ver _MENU_ entradas');
+define('DATATABLES_info', 'A Mostrar de _START_ a _END_ de _TOTAL_ entradas');
+define('DATATABLES_infoEmpty', 'A mostrar de 0 a 0 entradas de 0 entradas');
+define('DATATABLES_infoFiltered', '(filtrado de _MAX_ entradas totais)');
+define('DATATABLES_loadingRecords', 'A Carregar...');
+define('DATATABLES_zeroRecords', 'Nenhum registo correspondente encontrado');
+define('DATATABLES_emptyTable', 'Tabela sem dados disponíveis');
+define('DATATABLES_first', 'Primeira');
+define('DATATABLES_previous', 'Anterior');
+define('DATATABLES_next', 'Seguinte');
+define('DATATABLES_last', 'Última');
+define('DATATABLES_sortAscending', ': ativar para ordenar a coluna por ordem crescente ');
+define('DATATABLES_sortDescending', ': ativar para ordenar a coluna por ordem decrescente');
