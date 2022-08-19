bc6a02f399dcd7a304d118bfaf93c7866ef9ba8e
Charles <63302440+Sarrus1@users.noreply.github.com>
2022-08-19 13:33:24 +0200

    Fix: typo (#46)
    * Fix: french translation
    
    * Fix: missing translation
    
    * Update French.php

--- inc/languages/French.php
+++ inc/languages/French.php
@@ -63,7 +63,7 @@ define('TABLE_STAGED', 'Niveaux');
 define('TABLE_NULL', 'Null');
 define('TABLE_TODAY', 'Aujourd\'hui');
 define('TABLE_YESTERDAY', 'Hier');
-define('TABLE_DAYS_AGO', 'days ago');
+define('TABLE_DAYS_AGO', 'jours');
 define('TABLE_TOP_PLAYER_NAME', 'Nom du meilleur joueur');
 define('TABLE_TOP_TIME', 'Meilleur temps');
 define('TABLE_TOTAL_STAGE_COMPLETIONS', 'Nombre total de completions de niveaux');
@@ -121,7 +121,7 @@ define('LOADING_RECENT_MAP_RECORDS_LIST', 'Chargement de la liste des records r�
 define('LOADING_TOP_PLAYERS_LIST', 'Chargement de la liste des meilleurs joueurs');
 define('MAP_NOT_ADDED_PROPERLY', 'n\'ont pas été ajouté correctement au serveur, merci de contacter un admin');
 define('NOT_FOUND_IN_DB', 'n\'ont pas été trouvé dans la base de données');
-define('ALL_TIMES_ARE_IN', 'All times are in');
+define('ALL_TIMES_ARE_IN', 'Tous les temps sont chargés');
 
 define('DATATABLES_processing', 'Traitement en cours...');
 define('DATATABLES_search', 'Rechercher:');
