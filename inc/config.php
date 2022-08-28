a976a868a395a74067b870105856d81c38f4fe9f
Bara <mail@bara.dev>
2022-08-28 21:25:21 +0200

    German translation (#52)

--- inc/config.php
+++ inc/config.php
@@ -60,7 +60,7 @@
 */
 
     $settings_language_enable       = TRUE;         // Default: TRUE - To disable language selector change to FALSE.
-    $settings_language_default      = "English";    // Czech, Danish, English, French, Portuguese, Slovak, Turkish
+    $settings_language_default      = "English";    // Czech, Danish, English, French, German, Portuguese, Slovak, Turkish
     $settings_player_flags          = TRUE;         // Default: TRUE - To disable player flags change to FALSE
     $settings_maps_download_url     = '';           // To disable remain empty EXAMPLE: http://example.com/maps/
 
