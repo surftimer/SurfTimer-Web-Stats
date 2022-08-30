363e7d10ebce4a1c78fc9c97dbd8eab961c95384
Vanheden <smultronstigen12@gmail.com>
2022-08-30 19:58:00 +0200

    Update Swedish.php

--- inc/languages/Swedish.php
+++ inc/languages/Swedish.php
@@ -22,7 +22,7 @@ define('HOME_TOTAL_MAPS', 'Totalt antal Maps');
 define('HOME_TOTAL_BONUSES', 'Totalt antal Bonus');
 define('HOME_TOTAL_COMPLETIONS', 'Totalt Kartor Slutförda');
 define('HOME_HOURS_PLAYED', 'Antal Timmar Spelade');
-define('HOME_RECENT', 'Senaste 10 Maps Rekord');
+define('HOME_RECENT', 'Senaste Top 10 Maps Rekord');
 define('HOME_TOP_PLAYERS', 'Top 10 Spelare');
 define('HOME_TOP_WR', 'Top 10 WR Rekordhållare');
 define('HOME_TOP_BONUS_WR', 'Top 10 bonus WR Rekordhållare');
